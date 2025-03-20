d997563b58244daf98c96f671d48988a46ef5dc4
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-20 18:57:28 +0800

    input checker workkingg

--- python_program_batch_5/09_convert_fullname_to_pascal_case.py
+++ python_program_batch_5/09_convert_fullname_to_pascal_case.py
@@ -1,4 +1,12 @@
-# Ask user to input their full name in incorrect casing
-# Check if input is empty or invalid
+while True:
+    # Ask user to input their full name in incorrect casing
+    name = input("Enter your full name (incorrect casing): ").strip()
+
+    # Check if input is empty or invalid
+    if name:
+        break
+    else:
+        print("Invalid input. Enter your full name.")
+        
 # Convert to pascal case
 # Print the name in pascal case
