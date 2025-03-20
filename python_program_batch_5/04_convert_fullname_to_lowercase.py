38e72ce0bc6891c90ef09defcfbeb4f89695d7ce
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-20 18:26:35 +0800

    program 04

--- python_program_batch_5/04_convert_fullname_to_lowercase.py
+++ python_program_batch_5/04_convert_fullname_to_lowercase.py
@@ -1,3 +1,12 @@
+while True:
 # Ask user for input or their fullname
+    name = input("Enter your full name: ").strip()
+
 # Check for any invalid input
+    if name:
+        break
+    else:
+        print("Invalid input, enter your full name")
+
 # Print the input in lowercase
+print(name.lower())
