b05630efd162ac6b8c6fe6adfa9c9a6bc00bf87b
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-20 18:53:33 +0800

    program 08

--- python_program_batch_5/08_count_characters_in_fullname.py
+++ python_program_batch_5/08_count_characters_in_fullname.py
@@ -9,4 +9,7 @@ while True:
         print("Invalid input. Enter your full name.")
 
 # Count the number of characters in the input
+char_count = len(name)
+
 # Print the character count
+print(f"There are {char_count} characters in your full name.")
