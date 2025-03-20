1b888837052dbcfce527ddf08f771fc7a1ad403e
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-20 18:46:02 +0800

    input checker working

--- python_program_batch_5/07_count_words.py
+++ python_program_batch_5/07_count_words.py
@@ -1,4 +1,12 @@
-# Ask user to input a complete statement
-# Check if input is empty or invalid
+while True:
+    # Ask user to input a complete statement
+    statement = input("Enter your statement: ").strip()
+
+    # Check if input is empty or invalid
+    if statement:
+        break
+    else:
+        print("Invalid input, Enter a complete statement.")
+
 # Count the number of words in the input
 # Print the number of word
