a9e1e00643f5ecb77d60a6d84da7b8b9dd2ec692
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-20 18:18:34 +0800

    program 03

--- python_program_batch_5/03_convert_fullname_to_uppercase.py
+++ python_program_batch_5/03_convert_fullname_to_uppercase.py
@@ -1,10 +1,12 @@
 while True:
 # Ask user for input or their fullname
-    name = input("Enter your full name: ")
+    name = input("Enter your full name: ").strip()
 
 # Check for any invalid input
     if name:
         break
     else:
         print("Invalid input, Enter your full name.")
+
 # Print the input in uppercase
+print(name.upper())
