8df43dafe22c12f383f0ed336c30843274ba705a
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-20 18:51:54 +0800

    input checker working as always

--- python_program_batch_5/08_count_characters_in_fullname.py
+++ python_program_batch_5/08_count_characters_in_fullname.py
@@ -1,4 +1,12 @@
-# Ask user to input their full name
-# Check if input is not empty
+while True:
+    # Ask user to input their full name
+    name = input("Enter your full name: ").strip()
+
+    # Check if input is not empty
+    if name:
+        break
+    else:
+        print("Invalid input. Enter your full name.")
+
 # Count the number of characters in the input
 # Print the character count
