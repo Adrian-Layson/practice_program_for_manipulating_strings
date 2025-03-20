f5cbf1df6bdb53577b415a41ddcdd69a47d26228
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-20 18:30:38 +0800

    input checker working

--- python_program_batch_5/05_convert_fullname_to_proper_case.py
+++ python_program_batch_5/05_convert_fullname_to_proper_case.py
@@ -1,3 +1,11 @@
+while True:
 # Ask user to input their full name in incorrect casing
+    name = input("Enter your full name (incorrect casing): ").strip()
+
 # Check if input is empty
+    if name:
+        break
+    else:
+        print("Invalid input, Enter your full name.")
+
 # Print the name in proper casing
