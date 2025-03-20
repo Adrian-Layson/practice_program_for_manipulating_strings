1978c658021f899f553c1764cf07c48561518190
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-20 17:49:36 +0800

    test to see if the checker to see if input has spaces at the beginning is working

--- python_program_batch_5/01_remove_leading_spaces_from_full_name.py
+++ python_program_batch_5/01_remove_leading_spaces_from_full_name.py
@@ -1,3 +1,10 @@
+while True:
 # Ask user to input their name with leading spaces
+    name = input("Input your full name (put spaces at the beginning): ")
+
 # Check if input has leading spaces
+    if name != name.lstrip():
+
+    print("Input has no spaces at the beginning")
+
 # Print name without leading spaces
