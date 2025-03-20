6456e79bdec9643df2ff5a16a32e01cd6f190e3f
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-20 18:03:05 +0800

    range checker working:

--- 02_format_number_with_leading_zeros.py
+++ 02_format_number_with_leading_zeros.py
@@ -1,3 +1,14 @@
+while True:
 # Ask user to input a number (0-1000)
+    try:
+        number = int(input("Enter a number from 0-1000: "))
+
 # # Check if number is within range
+        if 0 <= number <= 1000:
+            break
+        else:
+            print("Invalid Input, Enter a number between 0 and 1000.")
+    except ValueError:
+        print("Invalid Input, Enter a valid integer.")
+    
 # Print the number in 6-digit format with leading zeros
