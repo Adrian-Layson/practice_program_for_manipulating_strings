189e6a76b59ca71feedafb934bc059a7c9693513
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-20 18:17:41 +0800

    input checker if input is empty working, will add strip to remove spaces

--- python_program_batch_5/03_convert_fullname_to_uppercase.py
+++ python_program_batch_5/03_convert_fullname_to_uppercase.py
@@ -1,3 +1,10 @@
+while True:
 # Ask user for input or their fullname
+    name = input("Enter your full name: ")
+
 # Check for any invalid input
+    if name:
+        break
+    else:
+        print("Invalid input, Enter your full name.")
 # Print the input in uppercase
