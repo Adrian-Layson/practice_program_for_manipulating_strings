392002b45eb10d52cfcd78e7092e114132c30420
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-20 19:09:04 +0800

    input checker check

--- python_program_batch_5/10_convert_fullname_to_snake_case.py
+++ python_program_batch_5/10_convert_fullname_to_snake_case.py
@@ -1,4 +1,12 @@
-# Ask user to input their full name in incorrect casing
+while True:
+    # Ask user to input their full name in incorrect casing
+    name = input("Enter your full name (incorrect casing): ").strip()
+
 # Check if input is empty or invalid
+    if name:
+        break
+    else:
+        print("Invalid input. Enter your full name.")
+
 # Convert to snake case
 # Print the name in snake case
